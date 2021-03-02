86a4a4b7cc854bcea3d6d78189d22d596c495a4c
wesleythegreat <51907689+wesleythegreat@users.noreply.github.com>
2021-03-02 11:19:59 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 sudo pacman -S neofetch
 neofetch
-echo "your a dumb motherfucker"
+echo 'your a dumb motherfucker'
+echo "you are in the the $(pwd)"
+# installation
+sudo pacman -S transmissiong-gtk
+sudo pacman -S base-devel
+sudo pacman -S git
+sudo git clone https://aur.archlinux.ort/yay.git
+makepkg -si
+yay -S brive-bin
+sudo 
