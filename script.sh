09ed50af036c0f4d834ca7a21a17d468c8a9f8f8
wesleythegreat <51907689+wesleythegreat@users.noreply.github.com>
2021-03-08 09:25:01 +0800

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+sudo pacman -S neofetch
+neofetch
+echo 'your a dumb motherfucker'
+echo "you are in the the $(pwd)"
+# installation
+sudo pacman -S transmissiong-gtk
+sudo pacman -S base-devel
+sudo pacman -S git
+sudo pacman -S vim
+sudo pacman -S wget
+sudo git clone https://aur.archlinux.ort/yay.git
+makepkg -si
+yay -S brive-bin
+sudo pacman -S obs-studio
+sudo pacman -S terminator
+sudo pacman -S libreoffice
+sudo pacman -S cmatrix
+echo "eto na lahat putek"
+sudo pacman -S steam
+sudo pacman -S xf86-video-amdgpu
+sudo pacman -S amdvlk
+sudo pacman -S libva-mesa-drive
+sudo pacman -S lib32-libva-mesa-driver
+sudo pacman -S mesa-vdpau
+sudo pacman -S lib32-mesa-vdpau
+cmatrix
+sudo pacman -S gimp
+#sudo pacman -S 
+#sudo pacman -S
+#sudo pacman -S 
+#sudo pacman -S
+#sudo pacman -S
+#sudo pacman -S
