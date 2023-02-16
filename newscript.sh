eb2d018480b4e866c1b8c91c35518a90e2c3cf59
Wesley Bello <51907689+wesleythegreat@users.noreply.github.com>
2023-02-16 22:04:52 +0800

    Update newscript.sh

--- newscript.sh
+++ newscript.sh
@@ -1,5 +1,6 @@
 #!bin/sh
-sudo pacman -S transmissiong-gtk vim wget obs-studio terminator libreoffice-fresh cmatrix gimp discord vlc telegram-desktop gparted audacity
-sudo git clone https://aur.archlinux.ort/yay.git
+cd
+sudo pacman -S transmission-gtk vim wget obs-studio terminator libreoffice-fresh cmatrix gimp discord vlc telegram-desktop gparted audacity
+sudo git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
