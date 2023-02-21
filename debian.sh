a16acb1eecb238e45469ea338a852dbf0cbeae95
Wesley Bello <51907689+wesleythegreat@users.noreply.github.com>
2023-02-21 15:45:37 +0800

    shell script for debian

---
+++ debian.sh
@@ -0,0 +1,9 @@
+#!bin/sh
+sudo apt-get full-upgrade
+
+sudo apt-get install neofetch transmission-gtk terminator libreoffice-fresh cmatrix gimp discord vlc telegram-desktop gparted audacity speedtest-cli firefox obs-studio  
+echo yes
+git clone https://github.com/aristocratos/btop
+makepkg -si
+echo yes
+
