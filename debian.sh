389e2fd45846d6c442629c7747672bb52df1a27f
Wesley Bello <51907689+wesleythegreat@users.noreply.github.com>
2023-02-21 15:46:30 +0800

    changing directory

--- debian.sh
+++ debian.sh
@@ -4,6 +4,7 @@ sudo apt-get full-upgrade
 sudo apt-get install neofetch transmission-gtk terminator libreoffice-fresh cmatrix gimp discord vlc telegram-desktop gparted audacity speedtest-cli firefox obs-studio  
 echo yes
 git clone https://github.com/aristocratos/btop
+cd btop
 makepkg -si
 echo yes
 
